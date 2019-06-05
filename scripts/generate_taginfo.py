5b480f20d4a8cea6a1a82cc056acb7c8bcf7f981
Phyks (Lucas Verney) <phyks@phyks.me>
2019-06-05 15:06:18 +0000

    Add a basic script to generate taginfo reporting

---
+++ scripts/generate_taginfo.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+"""
+Taken from https://github.com/cquest/osmfr-cartocss/blob/master/tuning/tuning.py
+"""
+import argparse
+import json
+import re
+
+import sql_metadata
+import yaml
+
+TAGS_RE = re.compile(r"tags->'([^ ]+?)'", re.IGNORECASE)
+OSM2PGSQL_TAGS = [
+    'access', 'addr:housename', 'addr:housenumber', 'addr:interpolation',
+    'admin_level', 'aerialway', 'aeroway', 'amenity', 'area', 'barrier',
+    'bicycle', 'brand', 'bridge', 'boundary', 'building', 'capital',
+    'construction text', 'covered', 'culvert', 'cutting', 'denomination text',
+    'disused', 'ele', 'embankment', 'foot', 'generator:source', 'harbour',
+    'highway', 'historic', 'horse', 'intermittent text', 'junction',
+    'landuse', 'layer', 'leisure', 'lock', 'man_made', 'military', 'motorcar',
+    'name', 'natural', 'office', 'oneway', 'operator', 'place', 'population',
+    'power', 'power_source text', 'public_transport text', 'railway', 'ref',
+    'religion', 'route', 'service', 'shop', 'sport', 'surface', 'toll',
+    'tourism', 'tower:type', 'tracktype', 'tunnel', 'water', 'waterway',
+    'wetland', 'width', 'wood'
+]
+
+
+taginfo = {
+    "data_format": 1,
+    "project": {
+        "name": "Keys used in CyclOSM mapnik style",
+        "description": "This files contains all keys used for rendering in CyclOSM carto style.",
+        "project_url": "https://github.com/cyclosm/cyclosm-cartocss-style",
+        "contact_name": "Lucas Verney (Phyks)",
+        "contact_email": "phyks+cyclosm@phyks.me"
+    },
+    "tags": []
+}
+
+parser = argparse.ArgumentParser(description='Export taginfo-like stats about the tags used.')
+parser.add_argument('yaml', type=str)
+args = parser.parse_args()
+
+with open(args.yaml, 'r') as fh:
+    yml = yaml.load(fh, Loader=yaml.FullLoader)
+
+tags = []
+for layer in yml['Layer']:
+    if 'table' not in layer['Datasource']:
+        continue
+    sql = layer['Datasource']['table']
+
+    # Get tags from columns
+    columns = sql_metadata.get_query_columns(sql)
+    columns = [x.split('.')[-1] for x in columns]
+    columns = [x for x in columns if x in OSM2PGSQL_TAGS]
+    tags.extend(columns)
+
+    # Get tags from tags->'*'
+    keys = TAGS_RE.findall(sql)
+    for key in keys:
+        tags.append(key)
+
+# Filter out duplicates
+tags = sorted(set(tags))
+
+for tag in tags:
+    taginfo["tags"].append({
+        "key": tag,
+    })
+
+print(json.dumps(taginfo, indent=4))
