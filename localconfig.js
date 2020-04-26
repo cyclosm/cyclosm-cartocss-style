216ec18f57df5a969ae63b2f363aa359341041d8
Phyks (Lucas Verney) <phyks@phyks.me>
2020-04-26 12:31:42 +0200

    Fix issue with normal render by using a dedicated localconfig

--- localconfig.js
+++ localconfig.js
@@ -1,23 +1,7 @@
-const fs = require('fs');
-
 exports.LocalConfig = function (localizer, project) {
-    project.mml.Stylesheet.push({
-        id: "palette-lite.mss",
-        data: fs.readFileSync("./palette-lite.mss", "utf-8"),
-    });
-
-    project.mml.Layer = project.mml.Layer.filter(function (l) {
-        if (l.class) {
-            if (l.class.match(/cyclosm-lite/)) {
-                return true;
-            }
-        }
-        return false;
-    });
-
     project.mml.Layer.map(function (l) {
         if (l.Datasource.type == "postgis") {
-            l.Datasource.table = "(SELECT *, 1 AS is_lite FROM " + l.Datasource.table + ") AS data";
+            l.Datasource.table = "(SELECT *, 0 AS is_lite FROM " + l.Datasource.table + ") AS data";
         }
     });
 
